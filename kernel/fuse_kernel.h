c48a497431e574ccb52cffacc10caaa0f030f6ce
Jean-Pierre André <jpandre@users.sourceforge.net>
2016-05-04 09:05:37 +0200

    Set the version as 1.3.1
    Set the version used internally consistent with the repository tags

--- kernel/fuse_kernel.h
+++ kernel/fuse_kernel.h
@@ -46,7 +46,7 @@
 #include <linux/major.h>
 #endif
 
-#define FUSE_SOLARIS_VERSION "1.1AR.2"
+#define FUSE_SOLARIS_VERSION "1.3.1"
 
 /** Version number of this interface */
 #define FUSE_KERNEL_VERSION 7
